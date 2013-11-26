26cc980d0a3e7485ec42ea289023e3969e5a80bf
jrmch30@gmail.com <jrmch30@gmail.com@7432d7ec-b6ae-7687-5833-6a7fb0efc1f3>
2013-11-26 14:17:17 +0000

    pd

---
+++ trunk/control/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+  </head>
+  <body>
+    <ul id="navbar">
+      <li> 
+        <a href="index.php"> Accueil </a>
+      </li>
+      <li>
+        <a href="./control/connexion.php"> Connexion </a>
+      </li>
+    </ul>
+  </body>
+</html>
