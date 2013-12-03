7ad208487dd940e69a8b04b78ce7e34b5afacce2
kchicaut@gmail.com <kchicaut@gmail.com@7432d7ec-b6ae-7687-5833-6a7fb0efc1f3>
2013-12-03 14:14:05 +0000

    bonjour

---
+++ trunk/model/connexionBD.php
@@ -0,0 +1,18 @@
+<?php
+		$hostname='infolimon'; //infolimon
+		$login='chicautk'; //login iut
+		$passwd='azerty13'; // votre passwd
+		$base='chicautk'; //login iut
+		$connect = mysql_connect($hostname,$login,$passwd) or die ("erreur de connexion");
+		mysql_select_db($base,$connect) or die ("erreur de connexion base");
+?>
+		
+	
+	
+	
+	
+	
+
+	
+	
+	
